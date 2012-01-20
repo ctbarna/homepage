1985b792937b72cf38a336d5daa241f079c52a9e
Chris Barna <chris@unbrain.net>
2012-01-20 15:42:47 -0800

    Farmers' market viz no longer takes hours to load.

--- js/farmers-map.js
+++ js/farmers-map.js
@@ -19,73 +19,36 @@ map.add(po.image()
         + "/b60fbbd51f17456794d2b0e4ed4f0d0c"
         + "/998/256/{Z}/{X}/{Y}.png").hosts(["a.", "b.", "c.", ""])));
 
-map.add(po.geoJson()
+var cities = po.geoJson()
         .features(city_points['features'])
         .id("cities")
-        .on("load", loadCities));
-
-
-var markets = po.geoJson()
-    .url("http://ctbarna.iriscouch.com/farmers_markets/_design/geo/_rewrite/data?bbox="+getBB())
-    .on("load", load)
-    .id("markets");
-map.add(markets);
-
+        .on("load", loadCities);
+map.add(cities);
 
 map.add(po.compass()
     .pan("none"));
 
 
 function loadCities(e) {
-for (var i = 0; i < e.features.length; i++) {
-    var feature = e.features[i];
-    feature.element.setAttribute("id", "city-"+i);
-  }
-}
-
-function load (e) {
-  function findDistance(city_coords1, market_coords1) {
-    var temp_x = city_coords1[0] - market_coords[0],
-        temp_y = city_coords1[1] - market_coords[1];
-    return Math.sqrt(temp_x * temp_x + temp_y * temp_y);
-  }
-  var counts = {};
-
-  for (var i = 0; i < e.features.length; i++) {
-    var distances = [];
-    var market_coords = e.features[i].data.geometry.coordinates;
-
-    for (var j = 0; j < city_points['features'].length; j++) {
-      var city_coords = city_points['features'][j]['geometry']['coordinates'];
-      distances.push(findDistance(city_coords, market_coords));
-    }
-
-    var least_distant = distances.indexOf(Array.min(distances));
-    if (least_distant+'' in counts) {
-      counts[least_distant+''] = counts[least_distant+'']+1;
-    } else {
-      counts[least_distant+''] = 1;
-    }
-  }
-
-  // Figure out the max count.
+  var counts = farmer_counts;
   var max_array = [];
   for (var key in counts) {
     max_array.push(counts[key]);
   }
   var max = Array.max(max_array);
 
-  var tile = e.tile, g = tile.element;
-  while(g.lastChild) g.removeChild(g.lastChild);
+  // var tile = e.tile, g = tile.element;
+  // while(g.lastChild) g.removeChild(g.lastChild);
 
-  for (var k = 0; k < city_points['features'].length; k++) {
-    var city = document.getElementById("city-"+k);
+  for (var i = 0; i < e.features.length; i++) {
+    var feature = e.features[i];
+    feature.element.setAttribute("id", "city-"+i);
 
-    if (document.getElementById("display-style").selectedIndex == 0) {
-      city.setAttribute('r', Math.log((counts[k+'']/max)+1)*125);
-    } else {
-      city.setAttribute('r', (counts[k+'']/max)*100);
-    }
+      if (document.getElementById("display-style").selectedIndex == 0) {
+        feature.element.setAttribute('r', Math.log((counts[i+'']/max)+1)*125);
+      } else {
+        feature.element.setAttribute('r', (counts[i+'']/max)*100);
+      }
   }
 }
 
@@ -94,5 +57,5 @@ function getBB (){
 }
 
 document.getElementById("display-style").onchange = function (e) {
-  markets.reload();
+  cities.reload();
 }
